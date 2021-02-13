ac0e65f7db51a5c5e8fe48dc6eabc7474ce1261f
furs0vn1ckita@gmail.com <furs0vn1ckita@gmail.com>
2021-02-13 17:31:20 +0300

    Solution for the 17th lab - 4th variant

---
+++ 17.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	const int n = 10;
+	const double max_rand = 2.0, min_rand = -2.0;
+	std::vector<double> arr(n); 
+	double sum = 0;
+	int first_neg = -1, second_neg = -1;
+
+	srand(time(NULL));
+	setlocale(0, "rus");
+
+	std::cout << "Массив: ";
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = (double)(rand()) / (double)RAND_MAX * (max_rand - min_rand) + min_rand;
+		std::cout << arr[i] << ' ';
+	}
+	for (int i = 0; i < n; i++)
+		sum += i % 2 == 1 ? arr[i] : 0;
+	std::cout << "\n\nСумма элементов с нечетными номерами: " << sum;;
+	sum = 0;
+	for (int i = 0; i < n; i++)
+		if (arr[i] < 0)
+		{
+			first_neg = i;
+			break;
+		}
+	for (int i = n - 1; i >= 0; i--)
+		if (arr[i] < 0)
+		{
+			second_neg = i;
+			break;
+		}
+	for (int i = first_neg + 1; i < second_neg; i++)
+		sum += arr[i];
+	std::cout << "\n\nСумма элементов между первым и последним отрицательным элементами: " << sum;
+
+	int s = 0;
+	for (int i = n - 1; i >= 0; i--)
+		if (abs(arr[i]) <= 1)
+		{
+			arr.erase(arr.begin() + i);
+			arr.push_back(0);
+			continue;
+		}
+
+	std::cout << "\n\nСжатый массив: ";
+	for (int i = 0; i < n; i++)
+		std::cout << arr[i] << ' ';
+
+	std::cout << "\n\n";
+	return 0;
+}
